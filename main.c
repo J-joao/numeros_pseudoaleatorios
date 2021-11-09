57834028cbf77d2050398ac1243fa5640cbf5de0
joao <83374960+J-joao@users.noreply.github.com>
2021-11-09 20:32:08 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef unsigned int u_int;
+
+//
+// Xn = P1 . Xn + P2 (mod M) 
+//
+int gerar_numeros (u_int X, u_int P1, u_int P2, u_int M)
+{
+    static int array[10];
+
+    for(int i = 0; i <= 10; i++)
+    {
+        array[i] = (P1 * X + P2) % M;          // Xn = P1 . Xn + P2 (mod M) 
+        printf("X%d = %d\n", i, array[i]);     // mostrar elementos no terminal
+        X = array[i];                          // mudar o valor de Xn para o resultado da equação
+    }
+}
+
+int main(void)
+{
+    gerar_numeros(79, 263, 71, 100);
+}
