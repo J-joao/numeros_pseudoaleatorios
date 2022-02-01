5b40b873f2b45518b403aa8b549603bad98aff45
joao <83374960+J-joao@users.noreply.github.com>
2022-01-31 22:03:02 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
-static uint *gerar_numeros (uint X, uint P1, uint P2, uint M)
-{
+static uint *gerar_numeros (uint X, uint P1, uint P2, uint M) {
     static uint array[10];
 
-    for(int i = 0u; i <= 10; i++)
-    {
+    for(int i = 0u; i <= 10; i++) {
         array[i] = (P1 * X + P2) % M; // Xn = P1 . Xn + P2 (mod M)
         X = array[i];                 // mudar o valor de X para o resultado da equação
     }
     return array;
 }
 
-int main(void)
-{
+int main(void) {
     uint *ptr = gerar_numeros(79, 263, 71, 100);
 
-    for(int i = 0u; i <= 10; i++)
-    {
+    for(int i = 0u; i <= 10; i++) {
         i != 10 ? printf("X%d  -> %d\n", i, *ptr++) : printf("X%d -> %d\n", i, *ptr++);
     }
     return 0;
